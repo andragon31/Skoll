da160c8cb9b393eab4d5a492fef70866ef372c97
Andragon <oscarcamarena31@hotmail.com>
2026-03-23 23:55:19 -0600

    chore: version bump to v4.0.0

--- cmd/skoll/main.go
+++ cmd/skoll/main.go
@@ -14,7 +14,7 @@ import (
 var openCodeFS embed.FS
 
 var (
-	version = "1.4.0"
+	version = "4.0.0"
 )
 
 func main() {
--- install.ps1
+++ install.ps1
@@ -6,7 +6,7 @@ $URL = "https://github.com/$REPO/releases/latest/download/$BIN"
 $INSTALL_DIR = "$env:LOCALAPPDATA\Programs\skoll"
 $EXE_PATH = "$INSTALL_DIR\skoll.exe"
 
-$VERSION = "v1.4.0"
+$VERSION = "v4.0.0"
 
 Clear-Host
 Write-Host "======================================" -ForegroundColor Cyan
